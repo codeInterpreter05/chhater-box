97b25d09e8ff187db789295c63ae2d87c1edef91
codeInterpreter05 <sarthupadhyay0507@gmail.com>
2024-08-24 19:24:23 +0530

    dev changes

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ import groupRoutes from './routes/groups.route.js';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 const databaseURL = process.env.DATABASE_URL;
  
 app.use(cors({
